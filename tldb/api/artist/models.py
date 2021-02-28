ed9036af684698fcbe79202596d06f52bbea5c0e
Dickie Field <me@rpf3.io>
2021-02-28 15:11:25 -0500

    feat: Add a POST route for creating a new artist
    This commit adds a new route for creating a new artist as well as a
    model for defining the artist fields.  The API payload is being run
    through the restx marshal function in order to ensure that only the
    defined fields are used.

--- tldb/api/artist/models.py
+++ tldb/api/artist/models.py
@@ -1,3 +1,11 @@
-from flask_restx import Namespace
+from flask_restx import Namespace, fields
 
 api = Namespace("artists")
+
+artist = api.model(
+    "Artist",
+    {
+        "id": fields.String(description="The ID of the artist"),
+        "name": fields.String(description="The name of the artist"),
+    },
+)
--- tldb/api/artist/resources.py
+++ tldb/api/artist/resources.py
@@ -1,6 +1,6 @@
-from flask_restx import Resource
+from flask_restx import Resource, marshal
 
-from tldb.api.artist.models import api
+from tldb.api.artist.models import api, artist
 
 
 @api.route("")
@@ -10,3 +10,12 @@ class Artists(Resource):
         List all artists
         """
         return []
+
+    @api.expect(artist)
+    def post(self):
+        """
+        Create a new artist
+        """
+        api_model = marshal(api.payload, artist)
+
+        return api_model
