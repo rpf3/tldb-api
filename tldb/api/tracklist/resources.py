9d55d67a440ab4be4907a590a51d7539a55071fc
Dickie Field <me@rpf3.io>
2021-03-01 22:23:45 -0500

    ref: Reference the database as an instance var
    This commit refactors the Flask API classes so that the database table
    references are stored on init as instance variables and the code doesn't
    need to be repeated in each method.

--- tldb/api/tracklist/resources.py
+++ tldb/api/tracklist/resources.py
@@ -6,12 +6,15 @@ from tldb.database import tables
 
 @api.route("")
 class Tracklists(Resource):
+    def __init__(self, res):
+        super().__init__(res)
+
+        self.table = tables.Tracklist()
+
     def get(self):
         """
         List all tracklists
         """
-        tracklist_table = tables.Tracklist()
-
-        database_response = tracklist_table.get()
+        database_response = self.table.get()
 
         return list(database_response)
