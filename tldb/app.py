7d1eb6959f9d5e8365f068ad72027968378712fe
Dickie Field <me@rpf3.io>
2021-03-02 18:35:20 -0500

    fix: Set up the database before Flask starts
    This commit moves the database setup into the server script so that it
    is guaranteed to be configured before the Flask app starts.

--- tldb/app.py
+++ tldb/app.py
@@ -1,10 +1,7 @@
 from flask import Flask
 
 from tldb.api import blueprint
-from tldb.database import setup as database
 
 app = Flask(__name__)
 
 app.register_blueprint(blueprint)
-
-database.create()
