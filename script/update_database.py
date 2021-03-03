426611372767836b1fea7e2340eb360be71c0029
Dickie Field <me@rpf3.io>
2021-03-03 17:43:33 -0500

    ref: Move the database init to the script dir
    This commit refactors how the database initialization works by moving it
    out of the main app directory and into the script directory.  I'd prefer
    to have just done with entirely with bash but for now Python will
    suffice.

---
+++ script/update_database.py
@@ -0,0 +1,39 @@
+import os
+
+from rethinkdb import r
+
+DATABASE_NAME = "tldb"
+
+
+def create_database(conn):
+    if r.db_list().contains(DATABASE_NAME).run(conn):
+        r.db_drop(DATABASE_NAME).run(conn)
+
+    r.db_create(DATABASE_NAME).run(conn)
+
+
+def create_artist_table(conn):
+    r.db(DATABASE_NAME).table_create("artist").run(conn)
+
+
+def create_track_table(conn):
+    r.db(DATABASE_NAME).table_create("track").run(conn)
+
+
+def create_tracklist_table(conn):
+    r.db(DATABASE_NAME).table_create("tracklist").run(conn)
+
+
+if __name__ == "__main__":
+    host = os.environ["RETHINKDB_HOST"]
+    port = os.environ["RETHINKDB_PORT"]
+
+    try:
+        conn = r.connect(host=host, port=port)
+
+        create_database(conn)
+        create_artist_table(conn)
+        create_track_table(conn)
+        create_tracklist_table(conn)
+    finally:
+        conn.close()
