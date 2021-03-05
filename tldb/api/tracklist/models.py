1c1603f2b0aedb3a41c5e25304053d65f4ccd02c
Dickie Field <me@rpf3.io>
2021-03-04 22:31:48 -0500

    feat: Store the track index
    This commit adds the index of the track to the tracklist model in order
    to make it explicit and not implicitly determined based on the array.

--- tldb/api/tracklist/models.py
+++ tldb/api/tracklist/models.py
@@ -2,13 +2,21 @@ from flask_restx import Namespace, fields
 
 api = Namespace("tracklists")
 
+track = api.model(
+    "Track",
+    {
+        "id": fields.String(description="The ID of the track"),
+        "index": fields.Integer(description="The index in the tracklist"),
+    },
+)
+
 tracklist = api.model(
     "Tracklist",
     {
         "id": fields.String(description="The ID of the tracklist", readonly=True),
         "name": fields.String(description="The name of the tracklist"),
         "artistId": fields.String(description="The ID of the artist"),
-        "tracks": fields.List(fields.String(description="The ID of the track")),
+        "tracks": fields.List(fields.Nested(track)),
     },
 )
 
