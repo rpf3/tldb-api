c66148bca19f68f1508f3aa20d6f2f823fe77b38
Dickie Field <me@rpf3.io>
2021-03-06 23:01:03 -0500

    feat: Add CORS support
    This commit adds CORS support from any origin for now so that local
    client applications can access the data.

--- tldb/app.py
+++ tldb/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 
 from tldb.api import blueprint
 
 app = Flask(__name__)
+
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 app.register_blueprint(blueprint)
