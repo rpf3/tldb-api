302e0a257348ac85814e8330f28b9711738036b1
Dickie Field <me@rpf3.io>
2021-03-24 23:43:37 -0400

    ref: Export the API blueprints as a module
    This commit exports the API blueprint objects so that they can be
    imported from the `api` module itself.

---
+++ tldb/api/__init__.py
@@ -0,0 +1,4 @@
+from tldb.api.artists import blp as artist_blueprint  # noqa: F401
+from tldb.api.imports import blp as import_blueprint  # noqa: F401
+from tldb.api.tracklists import blp as tracklist_blueprint  # noqa: F401
+from tldb.api.tracks import blp as track_blueprint  # noqa: F401
