e0c021fabde29cccd7388f1ff22367755dd19a8b
Dickie Field <me@rpf3.io>
2021-03-27 17:21:06 -0400

    fix: Use the new marshmallow models in imports
    This commit fixes all the bulk import code to use the new marshmallow
    models and schemas.  It also adds a function to the Track class to get
    a unique hash for a track based on the unique attributes other than the
    track's ID which can be used to simplify the import utilities.

--- tldb/models/__init__.py
+++ tldb/models/__init__.py
@@ -2,6 +2,7 @@ from tldb.models.artist import ArtistSchema, ArtistWriteSchema  # noqa: F401
 from tldb.models.track import TrackSchema, TrackWriteSchema  # noqa: F401
 from tldb.models.tracklist import (  # noqa: F401
     IndexedTrackSchema,
+    TracklistImportSchema,
     TracklistSchema,
     TracklistWriteSchema,
 )
--- tldb/models/track.py
+++ tldb/models/track.py
@@ -1,6 +1,8 @@
+import json
+
 from marshmallow import EXCLUDE, fields, post_load
 
-from tldb.models.artist import ArtistIdSchema, ArtistSchema
+from tldb.models.artist import ArtistIdSchema, ArtistSchema, ArtistWriteSchema
 from tldb.models.schema import BaseSchema
 
 
@@ -26,12 +28,28 @@ class RemixWriteSchema(RemixSchema):
     artist = fields.Nested(ArtistIdSchema)
 
 
+class RemixImportSchema(RemixSchema):
+    artist = fields.Nested(ArtistWriteSchema)
+
+
 class Track:
-    def __init__(self, name=None, artist=None, remix=None, id=None):
+    def __init__(self, name=None, artist=None, remix=None, id=None, original_id=None):
         self.id = id
         self.artist = artist
         self.name = name
         self.remix = remix
+        self.original_id = original_id
+
+    def get_unique_hash(self):
+        obj = {"name": self.name, "artistId": self.artist.id}
+
+        if self.remix is not None:
+            obj["remixName"] = self.remix.name
+            obj["remixArtistId"] = self.remix.artist.id
+
+        result = hash(json.dumps(obj, sort_keys=True))
+
+        return result
 
 
 class TrackSchema(BaseSchema):
@@ -39,6 +57,9 @@ class TrackSchema(BaseSchema):
     name = fields.String(description="The name of the track")
     artist = fields.Nested(ArtistSchema)
     remix = fields.Nested(RemixSchema, allow_none=True)
+    original_id = fields.String(
+        description="The ID of the original track", data_key="originalId"
+    )
 
     @post_load
     def create_track(self, data, **kwargs):
@@ -56,3 +77,11 @@ class TrackWriteSchema(TrackSchema):
 class TrackIdSchema(TrackSchema):
     class Meta:
         fields = ["id"]
+
+
+class TrackImportSchema(TrackSchema):
+    artist = fields.Nested(ArtistWriteSchema)
+    remix = fields.Nested(RemixImportSchema, allow_none=True)
+
+    class Meta:
+        exclude = ["id"]
